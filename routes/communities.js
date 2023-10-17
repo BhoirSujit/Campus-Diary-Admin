bb1a9929aee3221babc55c98f048ca2ff2eaede6
Sujit <bhoirsujit04>
2023-10-17 21:11:40 +0530

    communities ready

--- routes/communities.js
+++ routes/communities.js
@@ -3,64 +3,139 @@ const express = require('express')
 const router = express.Router();
 
 const db = require('../config')
-const comRef = db.collection("community");
+const Ref = db.collection("community");
+const pRef = db.collection("posts");
 
 router.use(express.static('public'));
 
 
-async function getComDataByID(id, afterLoad) {
-    try {
-      //getting data
-      const snapshot = await postsRef.where('id', '==', id).get()
-      afterLoad(snapshot) 
-  
-    }
-    catch (error) {
-      console.log(error);
-    }
+//db
+async function getDataByID(id, afterLoad) {
+  try {
+    //getting data
+    const snapshot = await Ref.where('id', '==', id).get()
+    afterLoad(snapshot)
+
   }
+  catch (error) {
+    console.log(error);
+  }
+}
 
+async function removeById(id, afterLoad) {
+  try {
+    //getting data
+    const snapshot = await Ref.doc(id).delete();
+    var jobskill_query = pRef.where('communityId', '==', id);
+jobskill_query.get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+    doc.ref.delete();
+    afterLoad(snapshot)
+  });
+});
+  
+    
 
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+async function getDataByQuery(campus, afterLoad) {
+  try {
+    //getting data //add .where('campus', '==', campus)
+    const snapshot = await Ref.where('campus', '==', campus).get()
+    afterLoad(snapshot)
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+var data = []
 
 router
-.get("/", (req, res) => {
+  .get("/", (req, res) => {
     const locals = {
-      UsersData  : []
+      UsersData: []
     }
     res.render("communities", locals)
   })
-.post('/',function(req, res){
+  .post('/', function (req, res) {
 
-    let cid = req.body.id;
+    if (req.body.type == 'q') {
+      let campus = req.body.campus;
+      //let sort = req.body.sort;
 
-    getComDataByID(cid, (snapshot) =>{
-        
-        var data = [];
+      console.log('getting data for community : ' + campus + " ");
 
-        console.log('snapshote are : '+snapshot);
+
+      getDataByQuery(campus, (snapshot) => {
+
+        data = [];
+
+        console.log('snapshote are : ' + snapshot);
         if (snapshot.empty) {
-            console.log('No matching documents.');
-            
-          } 
-        else
-          {
-            snapshot.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
-                data.push(doc.data());
-              });
-              
-           
-          }
-        res.send( {
-            m : pid,
-            postdata : data
+          console.log('No matching documents.');
+
+        }
+        else {
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            data.push(doc.data());
+
+          });
+
+
+        }
+        res.send({
+          postdata: data
         })
-    })
+      })
+    }
+    else if (req.body.type == 'i') {
+      let pid = req.body.id;
+
+      getDataByID(pid, (snapshot) => {
+
+        data = [];
+
+        console.log('snapshote are : ' + snapshot);
+        if (snapshot.empty) {
+          console.log('No matching documents.');
+
+        }
+        else {
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            data.push(doc.data());
+
+          });
 
 
-    
-        
+        }
+        res.send({
+          m: pid,
+          postdata: data
+        })
+      })
+    }
+    else if (req.body.type == "r") {
+      //
+      removeById(req.body.id, (snapshot) => {
+      })
+      
+
+    }
+
+
+
+
+
+
   })
-  
-  
-  module.exports = router
+
+
+module.exports = router
