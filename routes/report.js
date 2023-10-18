6cc8b655480b7f65a56ecee9d7f0ea4ca4ea3dab
Sujit <bhoirsujit04>
2023-10-18 22:30:50 +0530

    reports done

--- routes/report.js
+++ routes/report.js
@@ -3,9 +3,21 @@ const express = require('express')
 const router = express.Router();
 
 const db = require('../config')
-const postsRef = db.collection("posts");
+const Ref = db.collection("reports");
 
 router.use(express.static('public'));
+
+async function getDataByQuery(afterLoad) {
+  try {
+    //getting data //add .where('campus', '==', campus)
+    const snapshot = await Ref.orderBy('submitDate', 'DESC').get()
+    afterLoad(snapshot)
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
 router
 .get("/", (req, res) => {
@@ -14,6 +26,36 @@ router
     }
     res.render("report", locals)
   })
-  
+  .post('/', function (req, res) {
+
+    if (req.body.type == 'q') {
+
+      console.log('getting data for reports');
+
+
+      getDataByQuery((snapshot) => {
+
+        var data = [];
+
+        console.log('snapshote are : ' + snapshot);
+        if (snapshot.empty) {
+          console.log('No matching documents.');
+
+        }
+        else {
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            data.push(doc.data());
+
+          });
+
+
+        }
+        res.send({
+          postdata: data
+        })
+      })
+    }
+  })
 
 module.exports = router
